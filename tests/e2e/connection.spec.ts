573dd35163c396b5ae2e1f3a807e96fcc0ec56e6
Marko Milenkovic <markomilenkovic546@gmail.com>
2026-03-20 18:37:14 +0100

    Update imports

--- tests/e2e/connection.spec.ts
+++ tests/e2e/connection.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from './fixtures';
+import { expect, test } from '../fixtures';
 
 test.describe('Connection', () => {
    test.beforeEach(async ({ mintingPage }) => {
--- tests/e2e/minting-flow.spec.ts
+++ tests/e2e/minting-flow.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from './fixtures';
+import { expect, test } from '../fixtures';
 
 test.describe('Minting flow', () => {
     test.beforeEach(async ({ mintingPage }) => {
